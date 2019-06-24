46ac6f714e9c338ebaf8542f1f064e7d5cd75d05
moonlight <moonlight920@outlook.com>
2019-06-24 22:59:26 +0800

    finish tai ji

--- app/src/main/java/com/yf/viewpractice/TaiJiFragment.kt
+++ app/src/main/java/com/yf/viewpractice/TaiJiFragment.kt
@@ -2,10 +2,11 @@ package com.yf.viewpractice
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yf.viewpractice.view.TaiJiView
 
 class TaiJiFragment : Fragment() {
 
@@ -18,6 +19,14 @@ class TaiJiFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val taijiView = view.findViewById<TaiJiView>(R.id.taijiView)
+        taijiView.animate()
+            .rotation(35000f)
+            .setDuration(14000)
+//            .setInterpolator(AccelerateInterpolator())
+            .setStartDelay(1000)
+            .start()
     }
 
 }
--- app/src/main/java/com/yf/viewpractice/view/TaiJiView.kt
+++ app/src/main/java/com/yf/viewpractice/view/TaiJiView.kt
@@ -1,17 +1,16 @@
 package com.yf.viewpractice.view
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import com.yf.viewpractice.R
-import com.yf.viewpractice.debugLog
 
 class TaiJiView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     View(context, attrs, defStyleAttr) {
 
-    val RADIUS_OUTER = 100f
-    val RADIUS_MIDDLE = RADIUS_OUTER / 2
+    val RADIUS_OUTER = 300f
     val RADIUS_INNER = RADIUS_OUTER / 6
 
     val whitePaint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -26,12 +25,23 @@ class TaiJiView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
 
     }
 
+
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
 
+        canvas.save()
         canvas.translate(width / 2f, height / 2f)
+        canvas.rotate(30f)
         canvas.drawCircle(0f, 0f, RADIUS_OUTER, blackPaint)
-//        canvas.translate(-width / 2f, -height / 2f)
+        canvas.drawArc(-RADIUS_OUTER, -RADIUS_OUTER, RADIUS_OUTER, RADIUS_OUTER, 90f, 180f, true, whitePaint)
+        canvas.drawArc(-RADIUS_OUTER, -RADIUS_OUTER, RADIUS_OUTER, RADIUS_OUTER, 270f, 180f, true, blackPaint)
 
+        canvas.drawCircle(0f, -RADIUS_OUTER / 2,RADIUS_OUTER/2,whitePaint)
+        canvas.drawCircle(0f, RADIUS_OUTER / 2,RADIUS_OUTER/2,blackPaint)
+
+        canvas.drawCircle(0f, -RADIUS_OUTER / 2,RADIUS_OUTER/6,blackPaint)
+        canvas.drawCircle(0f, RADIUS_OUTER / 2,RADIUS_OUTER/6,whitePaint)
+//        canvas.translate(-width / 2f, -height / 2f)
+        canvas.restore()
     }
 }
