dcb4c62a6378da792839c9d1f27198e66b5f081d
yuefeng <moonlight920@outlook.com>
2019-07-01 14:45:14 +0800

    仿写 用户详情 界面

--- douyincopy/src/main/java/com/yf/douyincopy/home/UserInfoFragment.kt
+++ douyincopy/src/main/java/com/yf/douyincopy/home/UserInfoFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
 import com.yf.douyincopy.*
 import com.yf.douyincopy.bean.Card
 import com.yf.douyincopy.bean.Cards
@@ -14,6 +15,8 @@ import com.yf.douyincopy.bean.ItemBeanHolder
 import com.yf.douyincopy.bean.OnCardChangeListener
 import com.yf.douyincopy.home.userinfo.MyAdapter
 import kotlinx.android.synthetic.main.fragment_userinfo.*
+import kotlinx.android.synthetic.main.layout_header_userinfo.*
+import kotlinx.android.synthetic.main.layout_title_userinfo.*
 
 class UserInfoFragment : Fragment(), OnCardChangeListener {
 
@@ -35,6 +38,45 @@ class UserInfoFragment : Fragment(), OnCardChangeListener {
         Cards.registerCardChangeListener(this)
         recyclerViewUserInfo.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         recyclerViewUserInfo.adapter = MyAdapter(ItemBeanHolder.dataList)
+
+        onTitleAlphaChange(0f)
+        appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var tempAlpha = 0f
+            override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+                // 方便计算，偏移量改成正值
+                val offset = -verticalOffset.toFloat()
+                val changeStart = headerLayout.height.toFloat() * 3 / 5
+                val changeEnd = headerLayout.height.toFloat() * 4 / 5
+                if (offset > 0) {
+                    when {
+                        offset < changeStart -> {
+                            if (tempAlpha != 0f) {
+                                tempAlpha = 0f
+                                onTitleAlphaChange(tempAlpha)
+                            }
+                        }
+                        offset in changeStart..changeEnd -> {
+                            val scale = (offset - changeStart) / (changeEnd - changeStart)
+                            onTitleAlphaChange(scale)
+                            bgView.alpha = scale
+                        }
+                        offset > changeEnd -> {
+                            if (tempAlpha != 1f) {
+                                tempAlpha = 1f
+                                onTitleAlphaChange(tempAlpha)
+                            }
+                        }
+                    }
+                }
+            }
+
+        })
+    }
+
+    fun onTitleAlphaChange(alpha: Float) {
+        bgView.alpha = alpha
+        btnFollow.alpha = alpha
+        tvUserName.alpha = alpha
     }
 
     override fun onDestroyView() {
